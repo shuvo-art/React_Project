458443f09579d3e6051198f1b930e25d60f101dc
shuvo-art <shuvo83qn@gmail.com>
2024-01-09 13:49:39 +0600

    Create Own Business Card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 import Main from "./components/Main"
+import Info from "./components/Info"
 
 function App() {
   return (
       <div className="container">
           <Navbar />
           <Main />
+          <Info />
       </div>
   )
 }
